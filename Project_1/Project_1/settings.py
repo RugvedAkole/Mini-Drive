5c6d83be732dce8407530ca6ac741ece39eb7c57
RugvedAkole <akolerugved3242@gmail.com>
2026-04-26 11:36:32 +0530

    Update settings.py

--- Project_1/Project_1/settings.py
+++ Project_1/Project_1/settings.py
@@ -128,7 +128,8 @@ STATIC_URL = 'static/'
 
 # Static files (CSS, JS)
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files (The actual PDFs students upload)
 MEDIA_URL = '/media/'
